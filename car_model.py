8629a733b9d0a36c3c37e451630fccb067d25d63
Joohoon Kim <joohoonkim@Joohoons-MacBook-Pro.local>
2020-04-12 21:50:23 -0400

    Added research paper, and reformated parameters

--- car_model.py
+++ car_model.py
@@ -1,10 +1,24 @@
+"""
+car_model.py
+
+The Car Class represents the physical nature of the car.
+Phsyics model is based on Ackermann steering.
+
+Written by David Joohoon Kim
+joohoon.kim@outlook.com
+"""
 import pygame
 from pygame.math import Vector2
 from math import sin, cos, tan, radians, degrees, copysign, pi
 
+""" Vehicle Parameters """
+LENGTH = 30  #(0.1m)
+WIDTH = 15   #(0.1m)
+MASS = 500   #(kg)
+
 
 class Car:
-    def __init__(self, x, y, angle=90.0, width = 30, height = 15, max_steer=0, max_speed=5, max_accel=5.0):
+    def __init__(self, x, y, angle=90.0, length=LENGTH, width=WIDTH, max_steer=0, max_speed=5, max_accel=5.0):
         self.position = Vector2(x,y)
         self.prev_pos = Vector2(x,y)
         self.velocity = Vector2(0.0,0.0)
@@ -13,8 +27,8 @@ class Car:
         self.angle = angle
         self.speed = 0.0
         # Constants
-        self.width = width
-        self.height = height
+        self.length = LENGTH
+        self.width = WIDTH
 
         # Thresholds for input
         self.max_accel = max_accel
@@ -42,7 +56,6 @@ class Car:
         # Print Characteristics
 #        print("SPEED="+str(self.speed))
 #        print("STEERANGLE="+str(self.steer_angle))
-#        print("ANGLE="+str(self.angle))
 #        print("VEL="+str(self.velocity))
 #        print("POS="+str(self.position))
 #        print("ACCEL="+str(self.accel))
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+"""
+CarSimPy
+
+This a physics based car simulation ran on Python.
+Attempts to model the physics of a car based on several factors.
+The simulation runs on PyGame. (Current version 2.0.0dev6)
+
+Written by David Joohoon Kim
+joohoon.kim@outlook.com
+"""
 import os
 from math import sin, cos, tan, radians, degrees, copysign, pi
 import pygame
@@ -5,7 +15,6 @@ from pygame.math import Vector2
 
 # local import
 import car_model
-import obstacle_model
 
 # Get image of car
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -15,29 +24,64 @@ car_image = pygame.image.load(image_path)
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 GAME_TICKS = 60
-CAR_WIDTH = 30
-CAR_HEIGHT = 15
 
 class Game:
     def __init__(self):
+        """ initialize screen """
         pygame.init()
-        pygame.display.set_caption("Car Sim")
-        self.width = SCREEN_WIDTH
-        self.height = SCREEN_HEIGHT
-        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("CarSimPy")
+        self.car_start_pos_x = SCREEN_WIDTH/2
+        self.car_start_pos_y = SCREEN_HEIGHT/2
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.ticks = GAME_TICKS
         self.exit = False
         self.car_image = None
         self.obstacle_list = []
 
-    ''' processes the simulation '''
+    """ defines the controls of the car """
+    """ TODO::Refine Controls for 2 wheel steering """
+    def controls(self, car, dt, pressed):
+        if pressed[pygame.K_LEFT]:
+            car.steer_angle += 5
+        elif pressed[pygame.K_RIGHT]:
+            car.steer_angle -= 5
+        if pressed[pygame.K_UP]:
+            car.accel += 0.1
+        elif pressed[pygame.K_DOWN]:
+            car.accel -= 0.1
+        else:
+            car.accel = 0
+        car.accel = max(-car.max_accel, min(car.accel, car.max_accel))
+
+    """ enforces collision with object and boundaries """
+    """ TODO::Create Walls for the car to collide with """
+    def enforceBoundaries(self, car):
+        if(car.position.x > 640):
+            car.position.x = 640
+            car.speed = 0
+        if(car.position.x < 10):
+            car.position.x = 10
+            car.speed = 0
+        if(car.position.y > 470):
+            car.position.y = 470
+            car.speed = 0
+        if(car.position.y < 10):
+            car.position.y = 10
+            car.speed = 0
+
+    """ draws the screen and objects """
+    def draw(self, car):
+        self.car_image = pygame.transform.scale(car_image,(car.length,car.width))
+        rotated = pygame.transform.rotate(self.car_image, car.angle)
+        rect = rotated.get_rect()
+        self.screen.blit(rotated, car.position-(rect.width//2, rect.height//2))
+        pygame.display.flip()
+
+    """ processes the simulation """
     def run(self):
         # Create car model
-        car = car_model.Car(self.width/2, self.height/2, 0, CAR_WIDTH, CAR_HEIGHT)
-        obstacle = obstacle_model.Obstacle(self.width/4, self.height/2, 0, CAR_WIDTH, CAR_HEIGHT)
-
-        self.obstacle_list.append(obstacle)
+        car = car_model.Car(self.car_start_pos_x, self.car_start_pos_y)
 
         while not self.exit:
             # Convert time from milliseconds to seconds
@@ -52,95 +96,22 @@ class Game:
             pressed = pygame.key.get_pressed()
             self.controls(car, dt, pressed)
 
-            # Enfore map boundaries on the car
-            #self.enforceBoundaries(car, self.obstacle_list)
-
             # Logic
             car.update(dt)
-            for obs in self.obstacle_list:
-                obs.update(dt)
 
             ## Enfore map boundaries on the car
-            self.enforceBoundaries(car, self.obstacle_list)
+            self.enforceBoundaries(car)
 
             # Drawing
             self.screen.fill((0,0,0))
             self.draw(car)
-            self.draw(obstacle)
 
             # Update the clock (Called once per frame)
             self.clock.tick(self.ticks)
-        ''' End of while not self.exit '''
+        """ End of while not self.exit """
 
         pygame.quit()
 
-    ''' defines the controls of the car '''
-    def controls(self, car, dt, pressed):
-        if pressed[pygame.K_LEFT]:
-            car.steer_angle += 5
-        elif pressed[pygame.K_RIGHT]:
-            car.steer_angle -= 5
-        if pressed[pygame.K_UP]:
-            car.accel += 0.1
-        elif pressed[pygame.K_DOWN]:
-            car.accel -= 0.1
-        else:
-            car.accel = 0
-        car.accel = max(-car.max_accel, min(car.accel, car.max_accel))
-
-    def enforceBoundaries(self, car, obstacle_list):
-        if(car.position.x > 640):
-            car.position.x = 640
-            car.speed = 0
-        if(car.position.x < 10):
-            car.position.x = 10
-            car.speed = 0
-        if(car.position.y > 470):
-            car.position.y = 470
-            car.speed = 0
-        if(car.position.y < 10):
-            car.position.y = 10
-            car.speed = 0
-
-        for obs in obstacle_list:
-            self.intersect(car, obs)
-
-    def intersect(self, car, obs):
-        car_width = car.position.x-car.width
-        car_height = car.position.x-car.height
-        obs_width = obs.position.x-obs.width
-        obs_height = obs.position.y-obs.height
-#        print(car.position.x)
-#        print(obs.position.x)
-#        print(obs_width)
-        if(car.position.x <= obs.position.x and car.position.x >= obs_width and car.position.y <= obs.position.y and car.position.y >= obs_height):
-            #print("HI 1")
-            if(abs(car.position.x-obs.position.x) < abs(car.position.x-obs_width)):
-                car.position.x = obs.position.x
-            else:
-                car.position.x = obs_width
-            if(abs(car.position.y-obs.position.y) < abs(car.position.y-obs_width)):
-                car.position.y = obs.position.y
-            else:
-                car.position.y = obs_width
-        if(car_width <= obs.position.x and car_width >= obs_width and car_height <= obs.position.y and car_height >= obs_height):
-            #print("HI 2")
-            if(abs(car_width-obs.position.x) > abs(car_width-obs_width)):
-                car.position.x = obs.position.x
-            else:
-                car.position.x = obs_width
-            if(abs(car_height-obs.position.y) > abs(car_height-obs_width)):
-                car.position.y = obs.position.y
-            else:
-                car.position.y = obs_width
-
-    ''' Draws the screen and objects '''
-    def draw(self, car):
-        self.car_image = pygame.transform.scale(car_image, (CAR_WIDTH,CAR_HEIGHT))
-        rotated = pygame.transform.rotate(self.car_image, car.angle)
-        rect = rotated.get_rect()
-        self.screen.blit(rotated, car.position - (rect.width / 2, rect.height / 2))
-        pygame.display.flip()
 
 
 if __name__ == '__main__':
