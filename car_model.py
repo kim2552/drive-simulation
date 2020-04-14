c080de0357a25a99258d7b08abb5b20b63097a55
joohoonkim <joohoon.kim@outlook.com>
2020-04-13 23:24:21 -0400

    USing different physics paper. restructured car physics. starting from scratch

--- car_model.py
+++ car_model.py
@@ -9,44 +9,61 @@ joohoon.kim@outlook.com
 """
 import pygame
 from pygame.math import Vector2
-from math import sin, cos, tan, radians, degrees, copysign, pi
+from math import sin, cos, tan, radians, degrees, copysign, pi, sqrt
 
 """ Vehicle Parameters """
 LENGTH = 30  #(0.1m)
 WIDTH = 15   #(0.1m)
 MASS = 500   #(kg)
+C_DRAG = 10
+C_RR = 30*C_DRAG
 
 
 class Car:
-    def __init__(self,x,y,angle=90,length=LENGTH,width=WIDTH,max_steer=0,max_speed=5,max_accel=5.0):
-        self.position = Vector2(x,y)
-        self.velocity = Vector2(0.0,0.0)
-        self.force = 0.0
+    def __init__(self,x,y,orient=90,max_steer=0,max_speed=5,max_accel=5.0):
+        self.pos = Vector2(x,y)
+        self.vel = Vector2(0.0,0.0)
+        self.accel = Vector2(0.0,0.0)
+        self.engine_force = 0.0
         self.steer_angle = 0.0
-        self.angle = 0.0
-        # Constants
-        self.length = LENGTH
-        self.width = WIDTH
-        self.mass = MASS
+        self.orient = 0.0
 
-        # Thresholds for input
-        self.max_accel = max_accel
+        # Threshold Constants
         self.max_steer = max_steer
         self.max_speed = max_speed
 
     ''' Update the vehicle information '''
     def update(self, dt):
-        accel = self.force/self.mass
-        self.velocity.x = ((self.velocity.x*2)+accel*dt)/2
-        self.position.x += self.velocity.x*dt
+        F_tract = self.engine_force
+        speed=sqrt(self.vel.x*self.vel.x + self.vel.y*self.vel.y)
+        F_drag = -C_DRAG*speed         #TODO::Lookup how to do scalar/vector multiplication
+        F_rr = -C_RR*speed                        #Rolling Resistance C_rr ~= 30*C_drag
+        F_long = F_tract + F_drag + F_rr
 
-    http://hyperphysics.phy-astr.gsu.edu/hbase/carcr.html
-    def setReaction(self,react):
-        f = -(0.5*self.mass*(self.velocity.x*self.velocity.x))/self.length
-        self.force = f
+        self.accel.x = F_long / MASS
+        self.vel.x = self.vel.x + (self.accel.x*dt)
+        self.pos.x = self.pos.x + (self.vel.x*dt)
+        print("EngineForce=",self.engine_force)
+        print("F_long=",F_long)
+        print("accel=",self.accel)
+        print("velocity=",self.vel)
+        print("position=",self.pos)
 
-    def setForce(self,force):
-        self.force = force
 
-    def setSteerAngle(self,angle):
-        self.steer_angle = angle
+    def setEngineForce(self,f):
+        self.engine_force = f
+
+    def setSteerAngle(self,a):
+        self.steer_angle = a
+
+    def getLength(self):
+        return LENGTH
+
+    def getWidth(self):
+        return WIDTH
+
+    def getPosition(self):
+        return self.pos
+
+    def getOrientation(self):
+        return self.orient
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ CarSimPy
 This a physics based car simulation ran on Python.
 Attempts to model the physics of a car based on several factors.
 The simulation runs on PyGame. (Current version 2.0.0dev6)
+https://asawicki.info/Mirror/Car%20Physics%20for%20Games/Car%20Physics%20for%20Games.html
 
 Written by David Joohoon Kim
 joohoon.kim@outlook.com
@@ -37,7 +38,6 @@ class Game:
         self.ticks = GAME_TICKS
         self.exit = False
         self.car_image = None
-        self.obstacle_list = []
 
     """ defines the controls of the car """
     """ TODO::Refine Controls for 2 wheel steering """
@@ -45,32 +45,22 @@ class Game:
         if pressed[pygame.K_LEFT]:
             car.setSteerAngle(5)
         elif pressed[pygame.K_RIGHT]:
-            car.setSteerAngle(5)
-        if pressed[pygame.K_UP]:
-            car.setForce(100000)
-        elif pressed[pygame.K_DOWN]:
-            car.setForce(-100000)
+            car.setSteerAngle(-5)
         else:
-            car.setForce(0)
-
-    """ enforces collision with object and boundaries """
-    """ TODO::Create Walls for the car to collide with """
-    def enforceBoundaries(self, car):
-        if(car.position.x > 640):
-            car.setReaction(1)
-        if(car.position.x < 10):
-            car.setForce(0)
-        if(car.position.y > 470):
-            car.setForce(0)
-        if(car.position.y < 10):
-            car.setForce(0)
+            car.setSteerAngle(0)
+        if pressed[pygame.K_UP]:
+            car.setEngineForce(1000)
+        elif pressed[pygame.K_DOWN]:
+            car.setEngineForce(-1000)
+        else:
+            car.setEngineForce(0)
 
     """ draws the screen and objects """
     def draw(self, car):
-        self.car_image = pygame.transform.scale(car_image,(car.length,car.width))
-        rotated = pygame.transform.rotate(self.car_image, car.angle)
+        self.car_image = pygame.transform.scale(car_image,(car.getLength(),car.getWidth()))
+        rotated = pygame.transform.rotate(self.car_image, car.getOrientation())
         rect = rotated.get_rect()
-        self.screen.blit(rotated, car.position-(rect.width//2, rect.height//2))
+        self.screen.blit(rotated, car.getPosition()-(rect.width//2, rect.height//2))
         pygame.display.flip()
 
     """ processes the simulation """
@@ -90,9 +80,6 @@ class Game:
             # User input
             pressed = pygame.key.get_pressed()
             self.controls(car, dt, pressed)
-
-            ## Enfore map boundaries on the car
-            self.enforceBoundaries(car)
 
             # Logic
             car.update(dt)
